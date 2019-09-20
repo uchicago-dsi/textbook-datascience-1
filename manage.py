baa3f0f671d5fd7dac745a6a061f92189f46f0d1
Jesse London <jesselondon@gmail.com>
2019-09-20 16:13:57 -0500

    rounded out process, management and documentation of build & promote/deploy

--- manage.py
+++ manage.py
@@ -1,7 +1,12 @@
+import itertools
+import json
+import os
 import pathlib
 import random
 import re
 import string
+import sys
+import time
 
 from argcmdr import Local, localmethod
 from descriptors import cachedproperty
@@ -9,7 +14,7 @@ from plumbum import colors
 from plumbum.commands import ExecutionModifier
 
 
-# TODO: add this to argcmdr...!
+# TODO: add _SHH to argcmdr!
 
 class _SHH(ExecutionModifier):
     """plumbum execution modifier to ensure output is not echoed to terminal
@@ -33,7 +38,13 @@ ROOT_PATH = pathlib.Path(__file__).parent
 
 ENV_PATH = ROOT_PATH / '.env'
 
-TEXTBOOK_PATH = ROOT_PATH / 'textbook'
+TEXTBOOK_PATH = ROOT_PATH / 'textbook' / '_site'
+
+ANSI_ERASE = "\033[K"
+ANSI_UPN = "\033[{n}A"
+ANSI_BACKN = "\033[{n}D"
+
+BUILDS_URL = 'https://api.github.com/repos/uchicagods/textbook-datascience-1/pages/builds'
 
 
 def setup_env():
@@ -111,7 +122,7 @@ class Manage(Local):
             print('your textbook preview server is running at:' | colors.bold)
             print(
                 '\n\t',
-                "http://localhost:8008/jupyter-book/" | colors.underline,
+                "http://localhost:8008/" | colors.underline,
             )
             print()
             print("preview the published result there prior to "
@@ -122,6 +133,11 @@ class Manage(Local):
     def status(self):
         """list running development servers"""
         return self.local.FG, self.compose['ps']
+
+    @localmethod
+    def restart(self):
+        """restart development servers"""
+        return self.local.FG, self.compose['restart']
 
     @localmethod('--destroy', action='store_true', help="tear down servers")
     def stop(self, args):
@@ -146,6 +162,12 @@ class Manage(Local):
                  help="do not prompt for confirmation")
     @localmethod('-f', '--force', action='store_true',
                  help="override restriction against uncommitted promotion")
+    @localmethod('-r', '--rebased', action='store_true',
+                 help="force push against remote history")
+    @localmethod('--no-poll', action='store_false', default=True, dest='poll',
+                 help="do not poll build status (polling requires environ variable "
+                      "GH_CREDENTIALS with value of the form: "
+                      '"my-user-name:my-password-or-token")')
     # TODO: @alias('publish')
     def promote(self, args):
         """publish textbook content on github.io"""
@@ -180,20 +202,146 @@ class Manage(Local):
                 print('aborted' | colors.fatal)
                 return
 
-        # TODO: test
-        yield self.local.FG, self.local['git'][
-            'subtree',
-            'push',
-            '--prefix', 'textbook',
-            'origin',
-            'gh-pages',
-        ]
+        if args.execute_commands:
+            build0 = yield from self._get_build()
+        else:
+            build0 = None
+
+        relative_path = TEXTBOOK_PATH.relative_to(ROOT_PATH)
+        prefix_args = ('--prefix', relative_path)
+
+        if args.rebased:
+            (_retcode, root_sha, _stderr) = yield SHH, self.local['git'][
+                'subtree',
+                'split',
+                prefix_args,
+            ]
+
+            if root_sha is None:
+                root_sha = 'DRY-RUN'
+            else:
+                # trim newline
+                root_sha = root_sha.strip()
+
+            yield self.local.FG, self.local['git'][
+                'push',
+                'origin',
+                f'{root_sha}:gh-pages',
+                '--force',
+            ]
+        else:
+            try:
+                yield self.local.FG, self.local['git'][
+                    'subtree',
+                    'push',
+                    prefix_args,
+                    'origin',
+                    'gh-pages',
+                ]
+            except self.local.ProcessExecutionError:
+                print()
+                print('subtree-push failed' | colors.fatal)
+                print()
+                print("hint: if you're sure, force-push, with the manage-promote "
+                      "flag: -r / --rebased" | colors.yellow)
+                print()
+                raise
 
         if args.execute_commands:
             print()
             print("SUCCESS" | colors.success)
             print()
             print("review the published textbook at:")
-            # TODO: use pages.datascience.uchicago.edu (here and in README)?
-            print("\n\t" + 'https://uchicagods.github.io/uchicagods/textbook-datascience-1/' + '\n')
-            # TODO: could perhaps (optionally) poll the builds API to let user know status of build / when it's complete
+            print("\n\t" + ('https://ds1.datascience.uchicago.edu/' | colors.underline) + '\n')
+
+            if args.poll and build0 is not None:
+                build = yield from self._poll_until(build0, changed='url')
+
+                _print_build_status(build)
+
+                if build['status'] == 'building':
+                    # NOTE: may need to test for ANSI capability?
+
+                    # we've already printed two lines; go back to beginning to re-poll:
+                    print(ANSI_UPN.format(n=2), end='')
+
+                    # clear the line:
+                    print(ANSI_ERASE, end='\r')
+
+                    build1 = yield from self._poll_until(build, changed='status')
+
+                    print(ANSI_ERASE, end='\r')
+                    _print_build_status(build1)
+
+    def _get_build(self):
+        github_credentials = os.getenv('GH_CREDENTIALS')
+        if not github_credentials:
+            return None
+
+        (_retcode, builds_out, _stderr) = yield SHH, self.local['curl'][
+            '-u', github_credentials,
+            BUILDS_URL,
+        ]
+
+        if builds_out is None:
+            # dry run
+            return None
+
+        unexpected_message = (
+            ("unexpected api response" | colors.fatal) +
+            f"\n\nfrom {BUILDS_URL} --"
+            f"\n\n{builds_out}"
+        )
+
+        try:
+            builds = json.loads(builds_out)
+            return builds[0]
+        except IndexError:
+            return {}
+        except (TypeError, ValueError):
+            print(unexpected_message)
+            raise
+        except KeyError:
+            if builds.get('message') == "Not Found":
+                print('unexpected response (Not Found) -- verify GH_CREDENTIALS' | colors.fatal)
+            else:
+                print(unexpected_message)
+            raise
+
+    def _poll_until(self, build0, changed):
+        build = build0
+        spinner = itertools.cycle('\|/-')
+
+        sys.stdout.write('polling page build api ... ')
+        sys.stdout.write(next(spinner))
+        sys.stdout.flush()
+
+        while build.get(changed) == build0.get(changed):
+            if build is not build0:
+                sys.stdout.write(ANSI_BACKN.format(n=1))
+                sys.stdout.write(next(spinner))
+                sys.stdout.flush()
+                time.sleep(2)
+
+            build = yield from self._get_build()
+
+        print()
+        return build
+
+
+def _print_build_status(build):
+    status = build['status']
+
+    if status == 'building':
+        status_colored = status | colors.yellow
+    elif status == 'built':
+        status_colored = status | colors.green
+    elif status == 'errored':
+        status_colored = status | colors.fatal
+    else:
+        status_colored = status
+
+    print('status:', status_colored)
+
+    if status == 'errored':
+        print('message:', build['error']['message'])
